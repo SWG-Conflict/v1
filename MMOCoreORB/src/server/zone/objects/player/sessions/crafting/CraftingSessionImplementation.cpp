4ca11870b261ffbad414de6608348c9c91537e52
SWG-Dev <>
2021-04-13 22:29:59 -0500

    - Crafting time set at a flat 1 second

--- MMOCoreORB/src/server/zone/objects/player/sessions/crafting/CraftingSessionImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/player/sessions/crafting/CraftingSessionImplementation.cpp
@@ -1208,12 +1208,16 @@ void CraftingSessionImplementation::createPrototype(int clientCounter, bool crea
 
 		if (createItem) {
 
-			startCreationTasks(manufactureSchematic->getComplexity() * 0, false);
+			//startCreationTasks(manufactureSchematic->getComplexity() * 2, false);
+			startCreationTasks(1, false);
+
 
 		} else {
 
 			// This is for practicing
-			startCreationTasks(manufactureSchematic->getComplexity() * 0, true);
+			//startCreationTasks(manufactureSchematic->getComplexity() * 2, true);
+			startCreationTasks(1, true);
+
 			xp = round(xp * 1.05f);
 		}
 
