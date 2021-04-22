7caeaedffb6995634ffb8a5ee54060de52e9586f
SWG-Dev <>
2021-04-21 20:59:26 -0500

    Documented code changes

--- MMOCoreORB/src/server/zone/managers/mission/MissionManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/mission/MissionManagerImplementation.cpp
@@ -256,6 +256,8 @@ void MissionManagerImplementation::handleMissionAccept(MissionTerminal* missionT
 	}
 
 	//Limit to two missions (only one of them can be a bounty mission)
+	// MODIFICATION: RETRIBUTION
+	// if (missionCount >= 2 || (hasBountyMission && mission->getTypeCRC() == MissionTypes::BOUNTY)) {
 	if (missionCount >= 4 || (hasBountyMission && mission->getTypeCRC() == MissionTypes::BOUNTY)) {
 		StringIdChatParameter stringId("mission/mission_generic", "too_many_missions");
 		player->sendSystemMessage(stringId);
